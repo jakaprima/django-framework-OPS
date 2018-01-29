1fe9bd720d5f98fd29a32b4d0da8b917c995423a
jakaprima <jakaprima123@gamil.com>
2018-01-29 14:17:34 +0700

    test

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 # Create your models here.
+class Penulis(models.Model):
+	nama = models.CharField(max_length=200)
+	email = models.EmailField()
+
+	def __str__(self):
+		return self.nama
